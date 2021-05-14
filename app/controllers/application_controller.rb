d8a1aaddbfa2d851ed809adc68a48d0c438895e7
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-05-14 10:49:12 -0400

    "added mint ui, sessions to application controller config"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions unless test?
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(32) }
     set :public_folder, 'public'
     set :views, 'app/views'
   end
